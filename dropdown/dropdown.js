73a93ed7f8a28a652402e0041fce0b96a5e82f6b
Boris Okunskiy <i@savant.pro>
2015-06-19 16:19:44 +0300

    Added linter

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -1,28 +1,28 @@
 module.exports = function (app) {
 
-  app.directive('uiDropdown', function() {
+  app.directive('uiDropdown', function () {
 
     return {
       restrict: 'A',
       scope: true,
       controllerAs: 'dropdown',
-      controller: function($scope, $element) {
+      controller: function ($scope, $element) {
 
         var ctrl = this
           , $html = angular.element(document.documentElement);
 
         ctrl.active = false;
 
-        ctrl.toggle = function(ev) {
+        ctrl.toggle = function (ev) {
           ctrl.active = !ctrl.active;
           ev.stopPropagation();
         };
 
-        ctrl.show = function() {
+        ctrl.show = function () {
           ctrl.active = true;
         };
 
-        ctrl.hide = function() {
+        ctrl.hide = function () {
           ctrl.active = false;
         };
 
@@ -41,13 +41,13 @@ module.exports = function (app) {
         $html.on('click', onClick);
         $html.on('keyup', onKeyup);
 
-        $element.on('$destroy', function() {
+        $element.on('$destroy', function () {
           $html.off('click', onClick);
           $html.off('keyup', onKeyup);
         });
 
       }
-    }
+    };
 
   });
 
--- notices/notices.js
+++ notices/notices.js
@@ -1,13 +1,13 @@
-module.exports = function(app) {
+module.exports = function (app) {
 
   app.factory('notices', [ '$timeout',
-    function($timeout) {
+    function ($timeout) {
 
       return {
 
         queue: [],
 
-        add: function(text, kind, timeout) {
+        add: function (text, kind, timeout) {
           var notices = this;
           var notice = {
             id: Math.random().toString(36).substring(2),
@@ -15,13 +15,13 @@ module.exports = function(app) {
             kind: kind || 'info'
           };
           notices.queue.push(notice);
-          $timeout(function() {
+          $timeout(function () {
             notices.remove(notice);
           }, timeout == null ? 10000 : timeout);
           return this;
         },
 
-        remove: function(notice) {
+        remove: function (notice) {
           var id = typeof notice == 'string' ? notice : notice.id;
           for (var i = 0; i < this.queue.length; i++) {
             var n = this.queue[i];
@@ -33,46 +33,46 @@ module.exports = function(app) {
           return this;
         },
 
-        addInfo: function(text, timeout) {
+        addInfo: function (text, timeout) {
           return this.add(text, 'info', timeout);
         },
 
-        addSuccess: function(text, timeout) {
+        addSuccess: function (text, timeout) {
           return this.add(text, 'success', timeout);
         },
 
-        addWarn: function(text, timeout) {
+        addWarn: function (text, timeout) {
           return this.add(text, 'warn', timeout);
         },
 
-        addError: function(text, timeout) {
+        addError: function (text, timeout) {
           return this.add(text, 'error', timeout);
         },
 
-        clear: function() {
+        clear: function () {
           this.queue = [];
         }
 
-      }
+      };
     }
   ]);
 
   app.directive('uiNotices', [ 'notices',
-    function(notices) {
+    function (notices) {
       return {
         restrict: 'EA',
         template: '<div ng-class="[\'notice\', notice.kind]" ' +
           'ng-repeat="notice in notices.queue">{{notice.text}}</div>',
-        link: function($scope) {
+        link: function ($scope) {
           var $wnd = angular.element(window);
-          $wnd.on('keyup', function(ev) {
+          $wnd.on('keyup', function (ev) {
             if (ev.keyCode != 27)
               return;
             notices.clear();
             $scope.$digest();
           });
         }
-      }
+      };
     }
   ]);
 
